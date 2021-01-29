888a110251c4ee68f0e2925e903b9bb00aa20e3f
batsWTR <baptiste.wentzler@wanadoo.fr>
2021-01-29 16:43:16 +0100

    add names

--- prog_esp/sketch_jan06a/sketch_jan06a.ino
+++ prog_esp/sketch_jan06a/sketch_jan06a.ino
@@ -10,8 +10,8 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NB_LED, PIN, NEO_GRB + NEO_KHZ800);
 
 
 
-const char* ssid = "freeboxxxxxxx";
-const char* pass = "xxxxxxxxxxxxxx";
+const char* ssid = "freebox_RXCGSQ";
+const char* pass = "baptistewentzler";
 
 IPAddress staticIP(192,168,1,99);
 IPAddress gateway(192,168,1,1);
